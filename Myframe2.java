81ffdc58e60da28286baaab97e7390d5ff411692
Aryandwivedi123 <80156852+Aryandwivedi123@users.noreply.github.com>
2021-03-07 16:26:42 +0530

    Add files via upload

---
+++ Myframe2.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class Myframe2 extends Frame{
+	Myframe2(){
+		setVisible(true);
+		setSize(300,900);
+		setTitle("Extends Frame class");
+	}
+	public static void main(String[]args){
+    Myframe2 fr=new Myframe2();	
+	}	
+}
